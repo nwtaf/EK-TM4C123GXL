0044903b9d35d54bfa3ec959f3eac22a3b60f319
nwtaf <112997769+nwtaf@users.noreply.github.com>
2023-03-23 04:15:20 -0400

    Updated definitions for .c function calls

--- OBD2Scanner/LCD_Driver.h
+++ OBD2Scanner/LCD_Driver.h
@@ -10,26 +10,25 @@
 
 /*define useful symbolic names for LCD instructions/commands
   from table 6 of datasheet */
-#define Function_Set_4bit 0x28 //001010-- //handles font size and lines of LCD (cannot display two lines for 5x10 char font)
-#define Display_On        0x0F //00001111 //display on, cursor on, blinking cursor
-#define Entry_Mode_Set    0x06 //00000110 //increment by 1, no shift
-#define Clear_Display     0x01 //clear display and sets DDRAM address 0 in Address Counter (AC)
-#define Return_Home       0x02 //00000001- //sets DDRAM address 0 in AC. Returns display from shifted to original position. Takes 1.52ms
+#define FunctionSet4Bit     0x20 //Must be first command
+#define ClearDisplay        0x01 //sets DDRAM address 0 in AC. Returns display from shifted to original position. Takes 1.52ms
+#define ReturnHome          0x02 //see table 6 for description, dont forget extra long execution time (add delay)
+#define DisplayOn           0x0C
+#define MoveCursorStartRow2 0xC0
+#define MoveCursorStartRow1 0x80 //Check this, but it should work
+/*#define moveCursorRight     0x06
+#define moveCursorLeft      0x08
+#define shiftDisplayRight   0x1C
+#define shiftDisplayLeft    0x18
+#define cursorBlink         0x0F
+#define cursorOff           0x0C
+#define cursorOn            0x0E
+#define Function_set_8bit   0x38 //alternative first command */
 
-/*unchecked from online sources. Use table 6 of datasheet to check
-#define moveCursorRight   0x06
-#define moveCursorLeft    0x08
-#define shiftDisplayRight 0x1C
-#define shiftDisplayLeft  0x18
-#define cursorBlink       0x0F
-#define cursorOff         0x0C
-#define cursorOn          0x0E
-#define Function_set_8bit 0x38 //001110-- //ew
-#define FirstRow          0x80*/
 
 void LCD_init(void);  /* LCD initialization function */
 void pulseLCDEnable(void);
-void sendChar(char c);
-void sendInstruction(uint8_t instruction);
-void sendStr(char *str);
+void sendChar(char c); //writes ASCII character
+void sendInstruction(uint8_t instruction); //sends an instruction
+void sendStr(char *str); //sends a string (array of chars that is null ('\0') terminated)
 #endif /* LCD_Driver */
